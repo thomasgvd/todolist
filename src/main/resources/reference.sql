365262914b17c8eb827feee5c010e0a1adf0aacc
Thomas Gervraud <thomasgervraud96@hotmail.fr>
2020-12-04 10:34:59 +0100

    Removed in-app database schema & data initialization

--- src/main/resources/reference.sql
+++ src/main/resources/reference.sql
@@ -1,3 +1,38 @@
+--Initial schema & data. Only here for reference but the database is initialized outside of the app.
+
+CREATE TABLE app_role (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE app_user (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_name VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    active BOOLEAN NOT NULL
+);
+
+CREATE TABLE app_permission (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE user_role (
+    user_id BIGINT NOT NULL REFERENCES app_user(id),
+    role_id BIGINT NOT NULL REFERENCES app_role(id)
+);
+
+CREATE TABLE role_permission (
+    role_id BIGINT NOT NULL REFERENCES app_role(id),
+    permission_id BIGINT NOT NULL REFERENCES app_permission(id)
+);
+
+CREATE TABLE task (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    description VARCHAR,
+    user_id BIGINT NOT NULL REFERENCES app_user(id)
+);
+
 INSERT INTO app_role (id, name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO app_role (id, name) VALUES (2, 'ROLE_USER');
 
